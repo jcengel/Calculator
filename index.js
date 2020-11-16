42cf950d69601344addc4b94ae85ae3da474651d
JC Engel <72950421+jcengel@users.noreply.github.com>
2020-11-16 15:52:25 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+import {sum, subtract, multiply, divide} from "./calculator.js"
+
+let output = document.querySelector("#output")
+
+let firstNumber = 0
+let operation = null
+
+document.querySelectorAll("#calculator .number").forEach(button => {
+    button.addEventListener("click", event => {
+        let value = event.currentTarget.textContent
+        if (operation){
+            output.value = value
+        } else {
+            output.value += value
+        }
+    })
+})
+
+document.querySelectorAll("#calculator .operation").forEach(button => {
+    button.addEventListener("click", event => {
+        firstNumber = Number.parseInt(output.value)
+        operation = event.currentTarget.dataset.action
+    })
+})
+
+const equal = document.querySelector("#calculator .equal")
+equal.addEventListener("click", () => {
+    if (!operation){
+        return
+    }
+    let secondNumber = Number.parseInt(output.value, 10)
+
+    if (operation === "sum") {
+        output.value = sum(firstNumber, secondNumber)
+    } else if (operation === "subtract") {
+        output.value = subtract(firstNumber, secondNumber)
+    } else if (operation === "multiply") {
+        output.value = multiply(firstNumber, secondNumber)
+    } else if (operation === "divide") {
+        output.value = divide(firstNumber, secondNumber)
+    }
+    //reset operation
+    operation = null
+})
